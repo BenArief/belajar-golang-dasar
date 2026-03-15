bdd4d0a22037daeb0d6827d16ef330c157959723
BenArief <benarief29@gmail.com>
2026-03-15 15:06:36 +0700

    Variable in golang done

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	 name  := "Ben Arief"
+	fmt.Println(name)
+
+	name = "Windah Basudara"
+	fmt.Println(name)
+
+	//Multiple variable
+	var (
+		firstName = "Ben"
+		lastname = "Arief"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastname)
+}
