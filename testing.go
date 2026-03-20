edb73cdc302b23924f7e38862bafe5e8ef43af4e
BenArief <benarief29@gmail.com>
2026-03-20 19:24:34 +0700

    Looking back to array and slice

--- testing.go
+++ testing.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func test1() {
 	var halo string = "Ini angka 2"
 	fmt.Println(halo)
+	arrai := [5]int{1,5,3,6}
+	for i := 0; i < len(arrai); i++ {
+		fmt.Println("Value", i, ":", arrai[i])
+	}
 }
 
 func main(){
 	test1()
+	num := 3.15
+	fmt.Printf("%f", num)
 }
