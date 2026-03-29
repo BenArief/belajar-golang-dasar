3ba206d0ee375132ac7f5b8f8484f3c888d38dbf
BenArief <benarief29@gmail.com>
2026-03-29 18:55:17 +0700

    Asterisk Operator

---
+++ asterisk_operator.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := &address1 // pointer
+
+	address2.City = "Bandung"
+	fmt.Println(address1) // bandung
+	fmt.Println(address2) // bandung juga
+
+	// address2 = &Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	*address2 = Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	fmt.Println(address1)
+	fmt.Println(address2) // jakarta
+
+}
