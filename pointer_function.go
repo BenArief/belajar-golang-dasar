55e534669088efcfe286671eb94ed4223e9dc751
BenArief <benarief29@gmail.com>
2026-03-29 22:30:21 +0700

    Pointer in Function and Method

---
+++ pointer_function.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func changeAddressToIndonesia(address *Address) {
+	address.Country = "Indonesia"			
+}
+
+func main() {
+	address := Address{}
+	changeAddressToIndonesia(&address)
+
+	fmt.Println(address)
+}
