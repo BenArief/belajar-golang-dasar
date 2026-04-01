301a0fb02139fbcbc83c0d394adeccdd25151e2a
BenArief <benarief29@gmail.com>
2026-04-01 16:49:34 +0700

    exercise-slice

--- database/exercise.go
+++ database/exercise.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func main() {
 	//Variables
-	var myNum int = 90
-	var myWord string = "hello World"
-	var myBool bool = true
-	x := 12.75
+	// var myNum int = 90
+	// var myWord string = "hello World"
+	// var myBool bool = true
+	// x := 12.75
 
-	fmt.Println(myNum)
-	fmt.Println(myWord)
-	fmt.Println(myBool)
-	fmt.Printf("x: %.2f\n", x )
+	// fmt.Println(myNum)
+	// fmt.Println(myWord)
+	// fmt.Println(myBool)
+	// fmt.Printf("x: %.2f\n", x )
 
 	//Array
 	selamat := [4]string{"Pagi", "Siang", "Sore", "Malam"}
@@ -33,4 +33,24 @@ func main() {
 	//length of array
 	fmt.Println("Length of cars:", len(cars))
 	fmt.Println("Length of arr2:", len(arr2))
+
+	//slice
+	slice1 := []int{1,2,3} // kantong elastis
+	fmt.Println(len(slice1)) // isi nya berapa
+	fmt.Println(cap(slice1)) // kapasitas nya berapa, bisa lebih besar dari panjang nya
+
+	//bikin array dari slice
+	ary1 := [8]int{1,2,3,4,5,6,7,8}
+	slice_from_array := ary1[2:4]
+
+	fmt.Println("slice_from_array:", slice_from_array)
+	fmt.Println("Length: ", len(slice_from_array))
+	fmt.Println("Capacity: ", cap(slice_from_array))
+
+	//using make slice
+	myslice1 := make([]int, 5, 10)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
 }
